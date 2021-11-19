34cc33435c5250b7450536e901184b14812960fc
Barış Can Çifçi <17030220013@student.beykent.edu.tr>
2021-11-20 01:24:29 +0200

    Created components,pages folder and create Title component

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <img className="logo" src="../../images/logo.svg" alt="spacex-logo"></img>
+      <h1>LAUNCHES</h1>
+    </div>
+  );
+};
+
+export default Title;
