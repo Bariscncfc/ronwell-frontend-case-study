a6a87967af3aa985fcd15c9a9c57248267e16f90
Barış Can Çifçi <17030220013@student.beykent.edu.tr>
2021-11-20 11:01:51 +0200

    Apollo Client & Graphql added to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Title from "./components/Title";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Title />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../components/Navi/Title";
+
+const HomePage = () => {
+  return (
+    <div className="Home">
+      <Title />
+    </div>
+  );
+};
+
+export default HomePage;
