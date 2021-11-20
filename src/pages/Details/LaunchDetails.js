7967dbb0114e86e9986c16b6a0b7a2485cfdeb22
Barış Can Çifçi <17030220013@student.beykent.edu.tr>
2021-11-21 00:12:35 +0200

    Added LaunchDetail page styling

--- src/pages/Details/LaunchDetails.js
+++ src/pages/Details/LaunchDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import "./LaunchDetails.css";
 
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client";
@@ -32,17 +33,30 @@ const LaunchDetails = () => {
   console.log(data);
   return (
     <div className="details">
-      <h1>LaunchDetails Page</h1>
-      <Link to="/">
+      <Link to="/" className="back-btn">
         <i className="fas fa-arrow-left">{data.launch.mission_name}</i>
       </Link>
-      <p>Date:{data.launch.launch_date_utc}</p>
-      <a href={data.launch.links.video_link} className="btn">
-        Watch Here
-      </a>
+      <div className="detail-section">
+        <div>
+          <img
+            src={data.launch.links.flickr_images[0]}
+            alt="launch"
+            className="left-image"
+          ></img>
+        </div>
+        <div className="launch-info">
+          <h1>{data.launch.mission_name}</h1>
+          <p>Date:{data.launch.launch_date_utc}</p>
+          <br />
+          <a href={data.launch.links.video_link} className="btn">
+            Watch Here
+          </a>
+        </div>
+      </div>
+
       <h2>LAUNCH DETAİLS</h2>
       {data.launch.links.flickr_images.map((launch) => (
-        <img src={launch} alt="launchs"></img>
+        <img src={launch} alt="launchs" className="launch-image"></img>
       ))}
     </div>
   );
